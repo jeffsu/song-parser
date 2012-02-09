24bc6a85539732632e7f2c3de530f5cc062e5cfc
Kevin Huynh <kevinhuynh1@gmail.com>
2012-02-08 23:33:10 -0800

    base

--- index.js
+++ index.js
@@ -5,9 +5,11 @@ var SongParser = require('./song-parser');
 var parser = new SongParser();
 
 var files = fs.readdirSync("./samples");
+console.log("There are  " + files.length + " files");
 for (var i=0; i<files.length; i++) {
   var file = files[i];
   if (file.match(/\.txt$/)) {
+    console.log(file.name);
     var text = fs.readFileSync("./samples/" + file, 'utf8');
     console.log(parser.parse(text));
   }
